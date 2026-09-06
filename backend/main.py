80d6644e76ccc8acab4d25960b7a00b38ddd471f
Ulysses <ulyssesburdeniii@gmail.com>
2026-09-06 11:06:53 -0400

    feat: connect React frontend to FastAPI

--- backend/main.py
+++ backend/main.py
@@ -3,12 +3,24 @@ from redis_client import redis_client
 from redis_service import create_commit
 from redis_service import get_commit, update_commit, delete_commit
 
+
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from typing import Optional
 
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
+origins = [
+    "http://localhost:5174",
+]
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class CommitUpdate(BaseModel):
     repo_name: Optional[str] = None
